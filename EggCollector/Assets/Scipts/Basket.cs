0184043af5abe5a2e64d154f7c1de5ce62396620
Douglas Graham <dig2@rice.edu>
2020-05-23 14:35:00 -0700

    basket movement
    mouse now controls x position of basket

---
+++ EggCollector/Assets/Scipts/Basket.cs
@@ -0,0 +1,26 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Basket : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+         //gets current screen position of mouse
+        Vector3 mouse2D = Input.mousePosition;
+
+        //camera's z-position sets how far to push the mouse into 3D
+        mouse2D.z = -Camera.main.transform.position.z;
+
+        //Convert the point from 2D screen space into 3D game world space
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mouse2D);
+
+        //move the x position of the basket to that of the mouse
+        Vector3 pos = this.transform.position;
+        pos.x = mousePos3D.x;
+        this.transform.position = pos;
+    }
+}
