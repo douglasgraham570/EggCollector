f766af6820b9e229ab6daf565829d20ea497d377
Douglas Graham <dig2@rice.edu>
2020-05-24 17:48:54 -0700

    Added GUI Score Counter
    eggs score points, but red still needs to be accounted for

--- EggCollector/Assets/Scipts/Basket.cs
+++ EggCollector/Assets/Scipts/Basket.cs
@@ -2,9 +2,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Basket : MonoBehaviour
 {
+    public Text scoreGT;
+
+    private void Start()
+    {
+         //find reference to scoreCounter object
+        GameObject ScoreGO = GameObject.Find("ScoreCounter");
+
+        //assign scoreCounter's text to scoreGT
+        scoreGT = ScoreGO.GetComponent<Text>();
+
+        //initialize the starting score to "0"
+        scoreGT.text = "0";
+    }
 
     // Update is called once per frame
     void Update()
@@ -14,7 +28,7 @@ public class Basket : MonoBehaviour
 
         //camera's z-position sets how far to push the mouse into 3D
         mouse2D.z = -Camera.main.transform.position.z;
-
+          
         //Convert the point from 2D screen space into 3D game world space
         Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mouse2D);
 
@@ -33,6 +47,15 @@ public class Basket : MonoBehaviour
         if (collidedWith.tag == "Egg" || collidedWith.tag == "Red Egg")
         {
             Destroy(collidedWith);
-        } 
+        }
+
+        //parse the text of the score to an int
+        int score = int.Parse(scoreGT.text);
+
+        score += 1;
+
+        //convert score back to a string and display it
+        scoreGT.text = score.ToString();
+        
     }
 }
